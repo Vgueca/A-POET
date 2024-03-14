954e00185e1da45d096aba1b34d3202275441774
juliangarr <juliangarridoarana@gmail.com>
2024-03-14 11:52:21 +0100

    Environment Encoding 2.0

--- environment.py
+++ environment.py
@@ -6,11 +6,11 @@ class Environment:
                  # The first one, hand-picked,  must be 0
 
     # Inicializar un reproductor que mínimo tenga un tipo de casilla
-    def __init__(self, id = None, n_cells = 0, dim_rel = 0, 
+    def __init__(self, id = None, nRows = 0, nCols = 0, 
                  empty = 0, wall = 0, stone = 0, sand = 0, water = 0, grass = 0, mud = 0, bikini = 0, shoes = 0, charge = 0):
         self.name = 'e_' + str(id)
-        self.n_cells = n_cells
-        self.dim_rel = dim_rel
+        self.nRows = nRows
+        self.nCols = nCols
 
         self.empty = empty
         self.wall = wall
@@ -27,8 +27,9 @@ class Environment:
         child_id = Environment.count_ids
         Environment.count_ids += 1
 
-        n_cells = self.n_cells
-        dim_rel = self.dim_rel
+        nRows = self.nRows
+        nCols = self.nCols
+        
         empty = self.empty
         wall = self.wall
         stone = self.stone
@@ -41,14 +42,14 @@ class Environment:
         charge = self.charge
 
         #están un poco mal planteados los parametros, pensar mejor como codear y programar en consecuencia
-        if 'n_cells' in categories:
-            n_cells = self.n_cells + r.randint(-10, 10)
+        if 'nRows' in categories:
+            nRows = self.nRows + r.randint(-10, 10)
 
-        if 'dim_rel' in categories:
-            n_cells = self.n_cells + r.randint(-1, 1)
+        if 'nCols' in categories:
+            nRows = self.nRows + r.randint(-1, 1)
 
         if 'empty' in categories:
-            empty = np.round(self.empty + r.randint(0, 5)/n_cells) #Esta suma es una mierda
+            empty = np.round(self.empty + r.randint(0, 5)/nRows) #Esta suma es una mierda
             max_empty = 0.5
             if empty > max_empty:
                 empty = max_empty
