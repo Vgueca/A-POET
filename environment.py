b40c5e7e87d587a82cd3558667344c4a68b4c93e
Dani Cerezo <64024594+daniicereezzo@users.noreply.github.com>
2024-03-14 19:10:19 +0100

    Update environment.py

--- environment.py
+++ environment.py
@@ -6,11 +6,22 @@ class Environment:
                  # The first one, hand-picked,  must be 0
 
     # Inicializar un reproductor que mínimo tenga un tipo de casilla
-    def __init__(self, id = None, nRows = 0, nCols = 0, 
-                 empty = 0, wall = 0, stone = 0, sand = 0, water = 0, grass = 0, mud = 0, bikini = 0, shoes = 0, charge = 0):
+    def __init__(self, id = None, rows = 10, cols = 10,
+                 agent_row = 5, agent_col = 5, agent_orientation = 0,
+                 empty = 0, wall = 0, stone = 0, sand = 0, water = 0, grass = 0, mud = 0, bikini = 0, shoes = 0, charge = 0,
+                 seed = 3):
+        if id is None:
+            print("ERROR: Environment ID not provided") # RAISE ERROR
+            exit(1)
+
         self.name = 'e_' + str(id)
-        self.nRows = nRows
-        self.nCols = nCols
+        self.rows = rows
+        self.cols = cols
+
+        # agent's start parameters
+        self.agent_row = agent_row
+        self.agent_col = agent_col
+        self.agent_orientation = agent_orientation
 
         self.empty = empty
         self.wall = wall
@@ -22,13 +33,20 @@ class Environment:
         self.bikini = bikini
         self.shoes = shoes
         self.charge = charge
+        self.seed = seed
+
+        self.game_map = self.generate_game_map()
 
     def mutate(self, categories):
         child_id = Environment.count_ids
         Environment.count_ids += 1
 
-        nRows = self.nRows
-        nCols = self.nCols
+        rows = self.rows
+        cols = self.cols
+
+        agent_row = self.agent_row
+        agent_col = self.agent_col
+        agent_orientation = self.agent_orientation
         
         empty = self.empty
         wall = self.wall
@@ -40,16 +58,17 @@ class Environment:
         bikini = self.bikini
         shoes = self.shoes
         charge = self.charge
+        seed = self.seed
 
         #están un poco mal planteados los parametros, pensar mejor como codear y programar en consecuencia
-        if 'nRows' in categories:
-            nRows = self.nRows + r.randint(-10, 10)
+        if 'rows' in categories:
+            rows = self.rows + r.randint(-10, 10)
 
-        if 'nCols' in categories:
-            nRows = self.nRows + r.randint(-1, 1)
+        if 'cols' in categories:
+            cols = self.cols + r.randint(-1, 1)
 
         if 'empty' in categories:
-            empty = np.round(self.empty + r.randint(0, 5)/nRows) #Esta suma es una mierda
+            empty = np.round(self.empty + r.randint(0, 5)/rows) #Esta suma es una mierda
             max_empty = 0.5
             if empty > max_empty:
                 empty = max_empty
@@ -59,9 +78,30 @@ class Environment:
 
         #continuar con resto
 
-
         child = Environment(
-            name=child_id,
+            id=child_id,
+            rows=rows,
+            cols=cols,
+            agent_row=agent_row,
+            agent_col=agent_col,
+            agent_orientation=agent_orientation,
+            empty=empty,
+            wall=wall,
+            stone=stone,
+            sand=sand,
+            water=water,
+            grass=grass,
+            mud=mud,
+            bikini=bikini,
+            shoes=shoes,
+            charge=charge,
+            seed=seed
             )
 
         return child
+    
+    def get_game_map(self):
+        return self.game_map
+    
+    def generate_game_map(self):
+        pass
